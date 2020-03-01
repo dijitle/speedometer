ad7ac9a24bd93523bcf875284bb7b0e68be85e49
Arunas Slepikas <dijitle@gmail.com>
2020-03-01 15:19:41 -0600

    convert degrees to DMS

--- src/GPS.js
+++ src/GPS.js
@@ -58,17 +58,36 @@ export function drawGPS(ctx, w, strength) {
 }
 
 export function drawCoords(ctx, w, lat, lon) {
-  ctx.font = String(w / 40) + "px monospace";
+  ctx.font = String(w / 35) + "px monospace";
   ctx.strokeStyle = "#000000";
   ctx.fillStyle = "#000000";
 
-  let latDeg = Math.floor(lat);
+  let latDeg = Math.floor(Math.abs(lat));
+  let latMin = Math.floor((Math.abs(lat) - latDeg) * 60);
+  let latSec = ((Math.abs(lat) - latDeg - latMin / 60) * 3600).toFixed(2);
   let latDir = lat > 0 ? "N" : "S";
 
   let lonDeg = Math.floor(Math.abs(lon));
+  let lonMin = Math.floor((Math.abs(lon) - lonDeg) * 60);
+  let lonSec = ((Math.abs(lon) - lonDeg - lonMin / 60) * 3600).toFixed(2);
   let lonDir = lon > 0 ? "E" : "W";
 
-  let gpsText = latDeg + "° " + latDir + ", " + lonDeg + "° " + lonDir;
+  let gpsText =
+    latDeg +
+    "° " +
+    latMin +
+    "' " +
+    latSec +
+    '"' +
+    latDir +
+    ", " +
+    lonDeg +
+    "° " +
+    lonMin +
+    "' " +
+    lonSec +
+    '"' +
+    lonDir;
   ctx.fillText(
     gpsText,
     w / 2 - ctx.measureText(gpsText).width / 2,
